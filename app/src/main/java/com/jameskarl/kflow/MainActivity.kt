22f1b56a6efac032adcb52b6942762de7e3035f9
JamesKarl <kejun@wiseda.com>
2018-04-13 21:03:19 +0800

    rename JKFlow to flow.

--- app/src/main/java/com/jameskarl/kflow/MainActivity.kt
+++ app/src/main/java/com/jameskarl/kflow/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.jameskarl.kflow
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jameskarl.flow.Utils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        content.text = Utils.dog()
     }
 }
